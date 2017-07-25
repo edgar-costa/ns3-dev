923d57c55729533eda7933aa37cec5e0897335e2
edgarcosta92 <cedgar@student.ethz.ch>
2017-07-25 03:17:29 +0200

    dist

--- scripts/experiment.py
+++ scripts/experiment.py
@@ -22,12 +22,14 @@ parser.add_argument('-b', '--Bandwidth', help='',default=10)
 parser.add_argument('-t', '--SimulationTime', help='',default=10)
 parser.add_argument('-i', '--InterArrival', help='',default=1600)
 parser.add_argument('-d', '--Distribution', help='', default="distributions/enterprise_conga_scaled_100.csv")
+parser.add_argument('--LoadThreshold', help='', default=0.75)
+parser.add_argument('--RecordingTime', help='', default=1)
 
 args = parser.parse_args()
 
 #make dir
 output_name_ns3 = args.OutputFolder + "-" + str(args.Bandwidth) + "-" + str(args.InterArrival) + "-" + str(args.RunStep)
-folder_name = "outputs/" + output_name_ns3 + "-" + args.distribution.split("/")[1].split(".")[0]
+folder_name = "outputs/" + output_name_ns3 #+ "-" + args.Distribution.split("/")[1].split(".")[0]
 try:
     os.makedirs(folder_name)
 except:
@@ -39,7 +41,7 @@ for key,data in vars(args).iteritems():
     f.write(key + " : " + str(data) + "\n")
 f.close()
 
-cmd = 'time ./waf --run "fat-tree --OutputFolder={2} --LinkBandwidth={4}Mbps  --Delay=50 --QueueSize=100 --Protocol=TCP --K=4 --Monitor=false --Debug=true --Animation=false --SimulationTime={5} --SizeDistribution={7}  --IntraPodProb=0 --InterPodProb=1 --InterArrivalFlowTime={6} --ErrorRate={0} --ErrorLink=r_0_a0->r_c0 --EcmpMode={1} --FlowletGapScaling=2 --SimulationName={1}_{0} --RunStep={3} --TrafficPattern=distribution" &'
+cmd = 'time ./waf --run "fat-tree --OutputFolder={2} --LinkBandwidth={4}Mbps  --Delay=50 --QueueSize=100 --Protocol=TCP --K=4 --Monitor=false --Debug=true --Animation=false --SimulationTime={5} --SizeDistribution={7}  --IntraPodProb=0 --InterPodProb=1 --InterArrivalFlowTime={6} --ErrorRate={0} --ErrorLink=r_0_a0->r_c0 --EcmpMode={1} --FlowletGapScaling=2 --SimulationName={1}_{0} --RunStep={3} --TrafficPattern=distribution --StopThreshold={8} --RecordingTime={9}" &'
 
 
 
@@ -48,7 +50,7 @@ for test in tests:
     
     for error in errors:
         formated_cmd = cmd.format(error, test, output_name_ns3, args.RunStep, args.Bandwidth,
-                                   args.SimulationTime, args.InterArrival, args.Distribution)
+                                   args.SimulationTime, args.InterArrival, args.Distribution, args.LoadThreshold, args.RecordingTime)
 
         print formated_cmd
 
